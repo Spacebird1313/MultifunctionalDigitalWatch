a241fab320d7d4d2dd454740c3eb30c8f20609d6
Thomas Huybrechts <huybthomas@gmail.com>
2015-07-18 10:26:43 +0200

    Update AlarmSturing.vhd
    Indention correction

--- src/AlarmSturing.vhd
+++ src/AlarmSturing.vhd
@@ -14,31 +14,31 @@ use IEEE.NUMERIC_STD.ALL;
 entity AlarmSturing is
 	Port ( sysClk : in STD_LOGIC;
 	       reset : in STD_LOGIC;
-			 clkZet : in STD_LOGIC;
-			 clkDelay : in STD_LOGIC;															--Vertraging voor snel zetten (delay = 2x clkDelay)
-			 zetIn : in STD_LOGIC_VECTOR(1 downto 0);										--zet: (0) = zet Uur, (1) = zet Min													
-			 up : in STD_LOGIC;
-			 down : in STD_LOGIC;
-			 enableUit : out STD_LOGIC := '0';
-			 updown : out STD_LOGIC := '0';
-			 zet0 : out STD_LOGIC_VECTOR(3 downto 0) := "0000";
-			 zetUit : out STD_LOGIC_VECTOR(3 downto 0) := "0000");
+	       clkZet : in STD_LOGIC;
+	       clkDelay : in STD_LOGIC;															--Vertraging voor snel zetten (delay = 2x clkDelay)
+	       zetIn : in STD_LOGIC_VECTOR(1 downto 0);										--zet: (0) = zet Uur, (1) = zet Min													
+	       up : in STD_LOGIC;
+	       down : in STD_LOGIC;
+	       enableUit : out STD_LOGIC := '0';
+	       updown : out STD_LOGIC := '0';
+	       zet0 : out STD_LOGIC_VECTOR(3 downto 0) := "0000";
+	       zetUit : out STD_LOGIC_VECTOR(3 downto 0) := "0000");
 end AlarmSturing;
 
 architecture struct of AlarmSturing is
 	component ZetModule
 	Port ( sysClk : in STD_LOGIC;
 	       reset : in STD_LOGIC;
-			 enableIn : in STD_LOGIC;
-			 clkZet : in STD_LOGIC;
-			 clkDelay : in STD_LOGIC;															--Vertraging voor snel zetten (delay = 2x clkDelay)
-			 up : in STD_LOGIC;
-			 down : in STD_LOGIC;
-			 updownCount : in STD_LOGIC;														--Telrichting in defaultmodus tellen
-			 zetIn : in STD_LOGIC_VECTOR(3 downto 0);										--zet: (0) = zet Dis0, (1) = zet Dis1, (2) = zet Dis2, (3) = zet Dis3	
-			 enableUit : out STD_LOGIC;
-			 updown : out STD_LOGIC;
-			 zetUit : out STD_LOGIC_VECTOR(3 downto 0));	
+	       enableIn : in STD_LOGIC;
+	       clkZet : in STD_LOGIC;
+	       clkDelay : in STD_LOGIC;															--Vertraging voor snel zetten (delay = 2x clkDelay)
+	       up : in STD_LOGIC;
+	       down : in STD_LOGIC;
+	       updownCount : in STD_LOGIC;														--Telrichting in defaultmodus tellen
+	       zetIn : in STD_LOGIC_VECTOR(3 downto 0);										--zet: (0) = zet Dis0, (1) = zet Dis1, (2) = zet Dis2, (3) = zet Dis3	
+	       enableUit : out STD_LOGIC;
+	       updown : out STD_LOGIC;
+	       zetUit : out STD_LOGIC_VECTOR(3 downto 0));	
 	end component;
 
 	--Interne signalen
@@ -50,17 +50,17 @@ Signal enableIn_intern : STD_LOGIC := '0';
 begin
 	Inst_ZetModule : ZetModule
 		Port map( sysClk => sysClk,
-				    reset => reset,
-					 enableIn => enableIn_intern,
-					 clkZet => clkZet,
-					 clkDelay => clkDelay,
-					 up => up,
-					 down => down,
-					 updownCount => updownCount_intern,
-					 zetIn => zetIn_intern,
-					 enableUit => enableUit,
-					 updown => updown,
-					 zetUit => zetUit_intern);
+			  reset => reset,
+			  enableIn => enableIn_intern,
+			  clkZet => clkZet,
+			  clkDelay => clkDelay,
+			  up => up,
+			  down => down,
+			  updownCount => updownCount_intern,
+			  zetIn => zetIn_intern,
+			  enableUit => enableUit,
+			  updown => updown,
+			  zetUit => zetUit_intern);
 
 enableIn_intern <= '0';																			--Geen carry-in puls nodig (enkel zet-functionaliteit)
 
