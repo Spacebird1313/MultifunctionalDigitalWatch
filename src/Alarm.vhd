8a30978b00ef0b0515718cdfbf42e1466267d65d
Thomas Huybrechts <huybthomas@gmail.com>
2015-07-18 10:38:54 +0200

    Update Alarm.vhd
    Indent size change

--- src/Alarm.vhd
+++ src/Alarm.vhd
@@ -18,9 +18,9 @@ entity Alarm is
 	       clk4Hz : in STD_LOGIC;
 	       up : in STD_LOGIC;
 	       down : in STD_LOGIC;
-	       toggleAlarm : in STD_LOGIC;														--Alarm: uit-aan
+	       toggleAlarm : in STD_LOGIC;													--Alarm: uit-aan
 	       alarmUit : in STD_LOGIC;															--Stop het alarmsignaal vroegtijdig
-	       zet : in STD_LOGIC_VECTOR(1 downto 0);										--zet: (0) = instellen Uur, (1) = instellen Min												
+	       zet : in STD_LOGIC_VECTOR(1 downto 0);								--zet: (0) = instellen Uur, (1) = instellen Min												
 	       MM : in STD_LOGIC_VECTOR(7 downto 0);
 	       UU : in STD_LOGIC_VECTOR(7 downto 0);
 	       alarmActief : out STD_LOGIC := '0';
@@ -32,9 +32,9 @@ end Alarm;
 architecture struct of Alarm is
 	component Teller4x2DigitsCijfers
 	Generic ( minDis0 : integer := 0;
-		  minDis1 : integer := 0;
-		  minDis2 : integer := 0;
-		  minDis3 : integer := 0);
+		  			minDis1 : integer := 0;
+		  			minDis2 : integer := 0;
+		  			minDis3 : integer := 0);
 	Port ( sysClk : in STD_LOGIC;
 	       updown : in STD_LOGIC;
 	       enable : in STD_LOGIC;
@@ -57,7 +57,7 @@ architecture struct of Alarm is
 	       reset : in STD_LOGIC;
 	       clkZet : in STD_LOGIC;
 	       clkDelay : in STD_LOGIC;															--Vertraging voor snel zetten (delay = 2x clkDelay)
-	       zetIn : in STD_LOGIC_VECTOR(1 downto 0);										--zet: (0) = zet Uur, (1) = zet Min													
+	       zetIn : in STD_LOGIC_VECTOR(1 downto 0);							--zet: (0) = zet Uur, (1) = zet Min													
 	       up : in STD_LOGIC;
 	       down : in STD_LOGIC;
 	       enableUit : out STD_LOGIC := '0';
@@ -114,61 +114,61 @@ Signal dis3Out_intern : STD_LOGIC_VECTOR(7 downto 0) := "00000000";
 begin
 	Inst_Teller4x2DigitsCijfers : Teller4x2DigitsCijfers
 		Generic map( minDis0 => 0,
-			     minDis1 => 0,
-			     minDis2 => 0,
-			     minDis3 => 0)
+			     			 minDis1 => 0,
+			     			minDis2 => 0,
+			     			minDis3 => 0)
 		Port map( sysClk => sysClk,
-			  updown => updown_intern,
-			  enable => enable_intern,
-	             	  reset => reset,
-			  zet => zet_intern,
-			  zet0 => zet0_intern,
-			  dis0Max => dis0Max_intern,
-			  dis1Max => dis1Max_intern,
-			  dis2Max => dis2Max_intern,												--Dis2 niet gebruikt
-			  dis3Max => dis3Max_intern,												--Dis3 niet gebruikt
-			  ct => ct_intern,
-			  dis0Out => dis0Out_intern,
-			  dis1Out => dis1Out_intern,
-			  dis2Out => dis2Out_intern,												--Dis2 niet gebruikt
-			  dis3Out => dis3Out_intern);												--Dis3 niet gebruikt
-		Inst_AlarmSturing : AlarmSturing
+			  			updown => updown_intern,
+			  			enable => enable_intern,
+	         	  reset => reset,
+			  			zet => zet_intern,
+			  			zet0 => zet0_intern,
+						  dis0Max => dis0Max_intern,
+						  dis1Max => dis1Max_intern,
+						  dis2Max => dis2Max_intern,												--Dis2 niet gebruikt
+						  dis3Max => dis3Max_intern,												--Dis3 niet gebruikt
+						  ct => ct_intern,
+						  dis0Out => dis0Out_intern,
+						  dis1Out => dis1Out_intern,
+						  dis2Out => dis2Out_intern,												--Dis2 niet gebruikt
+						  dis3Out => dis3Out_intern);												--Dis3 niet gebruikt
+	Inst_AlarmSturing : AlarmSturing
 		Port map( sysClk => sysClk,
-					 reset => reset,
-					 clkZet => clk4Hz,
-					 clkDelay => clk2Hz,
-					 zetIn => zet,
-					 up => up,
-					 down => down,
-					 enableUit => enable_intern,
-					 updown => updown_intern,
-					 zet0 => zet0_intern,
-					 zetUit => zet_intern);
+						  reset => reset,
+						  clkZet => clk4Hz,
+						  clkDelay => clk2Hz,
+						  zetIn => zet,
+						  up => up,
+						  down => down,
+						  enableUit => enable_intern,
+						  updown => updown_intern,
+						  zet0 => zet0_intern,
+						  zetUit => zet_intern);
 	
 	Inst_AlarmModule : AlarmModule
 		Port map( sysClk => sysClk,
-			  reset => reset,
-			  toggleAlarm => toggleAlarm_intern,
-			  alarmUit => alarmUit,
-			  MM => MM,
-			  UU => UU,
-			  alarmMM => dis0Out_intern,
-			  alarmUU => dis1Out_intern,
-			  alarmActief => alarmActief,
-			  alarmSignaal => alarmSignaal_intern);
+			  			reset => reset,
+						  toggleAlarm => toggleAlarm_intern,
+						  alarmUit => alarmUit,
+						  MM => MM,
+						  UU => UU,
+						  alarmMM => dis0Out_intern,
+						  alarmUU => dis1Out_intern,
+						  alarmActief => alarmActief,
+						  alarmSignaal => alarmSignaal_intern);
 	
 	Inst_PulsmoduleToggleAlarm : Pulsmodule
 		Port map( sysClk => sysClk,
-			  reset => reset,
-			  signalIn => toggleAlarm,
-			  pulsOut => toggleAlarm_intern);
+						  reset => reset,
+						  signalIn => toggleAlarm,
+						  pulsOut => toggleAlarm_intern);
 	
 	Inst_AlarmSignaalPulsModule : SignalPulsModule
 		Port map( sysClk => sysClk,
-			  reset => reset,
-			  pulsFrequentie => clk2Hz,
-			  signalIn => alarmSignaal_intern,
-			  pulsOut => alarmSignaal);
+						  reset => reset,
+						  pulsFrequentie => clk2Hz,
+						  signalIn => alarmSignaal_intern,
+						  pulsOut => alarmSignaal);
 
 alarmMM <= dis0Out_intern;
 alarmUU <= dis1Out_intern;
