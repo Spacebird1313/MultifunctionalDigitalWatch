fd04f116091cd47ae2300fc8703a3bbf2f1e6804
Thomas Huybrechts <huybthomas@gmail.com>
2015-07-21 21:35:56 +0200

    Update TimerModule.vhd
    Indent correction

--- src/TimerModule.vhd
+++ src/TimerModule.vhd
@@ -13,12 +13,12 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity TimerModule is
 	Port ( sysClk : in STD_LOGIC;
-			 reset : in STD_LOGIC;
-			 enableIn : in STD_LOGIC;
-			 instelmodus : in STD_LOGIC_VECTOR(1 downto 0);
-			 SS : in STD_LOGIC_VECTOR(7 downto 0);
-			 MM : in STD_LOGIC_VECTOR(7 downto 0);
-			 enableOut : out STD_LOGIC := '0');
+	       reset : in STD_LOGIC;
+	       enableIn : in STD_LOGIC;
+	       instelmodus : in STD_LOGIC_VECTOR(1 downto 0);
+	       SS : in STD_LOGIC_VECTOR(7 downto 0);
+	       MM : in STD_LOGIC_VECTOR(7 downto 0);
+	       enableOut : out STD_LOGIC := '0');
 end TimerModule;
 
 architecture Behavioral of TimerModule is
