6787ff410ca955e290850d5f8c8ec4e3d438f8cc
Thomas Huybrechts <huybthomas@gmail.com>
2015-07-21 19:07:01 +0200

    Update PulsToSelectedModule.vhd
    Indent correction

--- src/PulsToSelectedModule.vhd
+++ src/PulsToSelectedModule.vhd
@@ -13,23 +13,23 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity PulsToSelectedModule is
 	Port ( sysClk : in STD_LOGIC;
-			 reset : in STD_LOGIC;
-			 selectedModus : in STD_LOGIC_VECTOR(4 downto 0);
-			 pulsIn : in STD_LOGIC_VECTOR(2 downto 0);
-			 pulsOutM0 : out STD_LOGIC_VECTOR(2 downto 0) := "000";						--Module 0
-			 pulsOutM1 : out STD_LOGIC_VECTOR(2 downto 0) := "000";						--Module 1
-			 pulsOutM2 : out STD_LOGIC_VECTOR(2 downto 0) := "000";						--Module 2
-			 pulsOutM3 : out STD_LOGIC_VECTOR(2 downto 0) := "000";						--Module 3
-			 pulsOutM4 : out STD_LOGIC_VECTOR(2 downto 0) := "000");						--Module 4
+	       reset : in STD_LOGIC;
+	       selectedModus : in STD_LOGIC_VECTOR(4 downto 0);
+	       pulsIn : in STD_LOGIC_VECTOR(2 downto 0);
+	       pulsOutM0 : out STD_LOGIC_VECTOR(2 downto 0) := "000";				--Module 0
+	       pulsOutM1 : out STD_LOGIC_VECTOR(2 downto 0) := "000";				--Module 1
+	       pulsOutM2 : out STD_LOGIC_VECTOR(2 downto 0) := "000";				--Module 2
+	       pulsOutM3 : out STD_LOGIC_VECTOR(2 downto 0) := "000";				--Module 3
+	       pulsOutM4 : out STD_LOGIC_VECTOR(2 downto 0) := "000");				--Module 4
 			 
 end PulsToSelectedModule;
 
 architecture Behavioral of PulsToSelectedModule is
 
-Type state is (state1, state2, state3, state4, state5);									--5 toestanden
+Type state is (state1, state2, state3, state4, state5);						--5 toestanden
 Signal presentState, nextState : state := state1;
 
-begin																										--Finite state machine (Mealy - uitgangen afhankelijk van de huidige stand en ingangen)
+begin												--Finite state machine (Mealy - uitgangen afhankelijk van de huidige stand en ingangen)
 StateRegister : process(sysClk)
 	begin
 		if rising_edge(sysClk) then
@@ -88,20 +88,19 @@ Uitgangen : process(presentState, pulsIn)
 NxtState : process(selectedModus)
 	begin
 		case selectedModus is
-			when "00001" =>																			--Ingang naar module 0 schakelen
+			when "00001" =>								--Ingang naar module 0 schakelen
 				nextState <= state1;
-			when "00010" =>																			--Ingang naar module 1 schakelen
+			when "00010" =>								--Ingang naar module 1 schakelen
 				nextState <= state2;
-			when "00100" =>																			--Ingang naar module 2 schakelen
+			when "00100" =>								--Ingang naar module 2 schakelen
 				nextState <= state3;
-			when "01000" =>																			--Ingang naar module 3 schakelen
+			when "01000" =>								--Ingang naar module 3 schakelen
 				nextState <= state4;
-			when "10000" =>																			--Ingang naar module 4 schakelen
+			when "10000" =>								--Ingang naar module 4 schakelen
 				nextState <= state5;
-			when others =>																				--Onbekende toestand: Ingang naar module 0 schakelen
+			when others =>								--Onbekende toestand: Ingang naar module 0 schakelen
 				nextState <= state1;
 		end case;
 	end process;
-																									
-end Behavioral;
 
+end Behavioral;
