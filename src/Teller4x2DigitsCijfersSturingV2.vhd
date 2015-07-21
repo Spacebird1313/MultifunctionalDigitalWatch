373b526e8b913e2915248834b3a6eddbdd37b974
Thomas Huybrechts <huybthomas@gmail.com>
2015-07-21 19:18:49 +0200

    Update Teller4x2DigitsCijfersSturingV2.vhd
    Indent correction

--- src/Teller4x2DigitsCijfersSturingV2.vhd
+++ src/Teller4x2DigitsCijfersSturingV2.vhd
@@ -13,18 +13,18 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Teller4x2DigitsCijfersSturingV2 is
 	Port ( sysClk : in STD_LOGIC;
-			 enable : in STD_LOGIC;
-			 reset : in STD_LOGIC;
-			 updownIn : in STD_LOGIC;
-			 disZet : in STD_LOGIC_VECTOR(3 downto 0);
-			 disCt : in STD_LOGIC_VECTOR(2 downto 0);
-			 updownUit : out STD_LOGIC := '0';
-			 disEnable : out STD_LOGIC_VECTOR(3 downto 0) := "0000");		 
+	       enable : in STD_LOGIC;
+	       reset : in STD_LOGIC;
+	       updownIn : in STD_LOGIC;
+	       disZet : in STD_LOGIC_VECTOR(3 downto 0);
+	       disCt : in STD_LOGIC_VECTOR(2 downto 0);
+	       updownUit : out STD_LOGIC := '0';
+	       disEnable : out STD_LOGIC_VECTOR(3 downto 0) := "0000");		 
 end Teller4x2DigitsCijfersSturingV2;
 
 architecture Behavioral of Teller4x2DigitsCijfersSturingV2 is
 
-Type state is (state1, state2);																	--2 toestanden
+Type state is (state1, state2);								--2 toestanden
 Signal presentState, nextState : state := state1;
 Signal enable_intern : STD_LOGIC := '0';
 Signal disZet_intern : STD_LOGIC_VECTOR(3 downto 0) := "0000";
@@ -63,7 +63,7 @@ updownBuffer : process(sysClk)
 		end if;
 	end process;
 
-StateRegister : process(sysClk)																	--Finite state machine (Mealy - uitgangen afhankelijk van de huidige stand en ingangen)
+StateRegister : process(sysClk)								--Finite state machine (Mealy - uitgangen afhankelijk van de huidige stand en ingangen)
 	begin
 		if rising_edge(sysClk) then
 			if reset = '1' then
@@ -103,7 +103,7 @@ Uitgangen : process(presentState, disZet_intern, disCt, enable_intern)
 				else
 					disEnable(3) <= '0';
 				end if;			
-			when others =>																				--Onbekende toestand: stop tellen
+			when others =>							--Onbekende toestand: stop tellen
 				disEnable <= "0000";
 		end case;
 	end process;
@@ -111,22 +111,21 @@ Uitgangen : process(presentState, disZet_intern, disCt, enable_intern)
 NxtState : process(presentState, disZet)
 	begin
 		case presentState is
-			when state1 =>																				--Modus: default tellen
+			when state1 =>							--Modus: default tellen
 				if disZet = "0000" then
 					nextState <= state1;
 				else
 					nextState <= state2;
 				end if;
-			when state2 =>																				--Modus: instellen
+			when state2 =>							--Modus: instellen
 				if disZet = "0000" then
 					nextState <= state1;
 				else
 					nextState <= state2;
 				end if;
-			when others =>																				--Onbekende toestand: default tellen
+			when others =>							--Onbekende toestand: default tellen
 				nextState <= state1;
 		end case;
 	end process;
 	
 end Behavioral;
-
