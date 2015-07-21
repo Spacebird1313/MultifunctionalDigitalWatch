e7ef591ef2b3ad1aa9c1fb0121810ce1b0dfa955
Thomas Huybrechts <huybthomas@gmail.com>
2015-07-21 22:10:34 +0200

    Update WerkingsmodeSelector.vhd
    Indent correction

--- src/WerkingsmodeSelector.vhd
+++ src/WerkingsmodeSelector.vhd
@@ -13,9 +13,9 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity WerkingsmodeSelector is
 	Port ( sysClk : in STD_LOGIC;
-			 nextMode : in STD_LOGIC;
+	       nextMode : in STD_LOGIC;
 	       reset : in STD_LOGIC;
-			 currentMode : out STD_LOGIC_VECTOR(4 downto 0) := "00001");
+	       currentMode : out STD_LOGIC_VECTOR(4 downto 0) := "00001");
 end WerkingsmodeSelector;
 
 architecture Behavioral of WerkingsmodeSelector is
@@ -23,7 +23,7 @@ architecture Behavioral of WerkingsmodeSelector is
 Type state is (state1, state2, state3, state4, state5);				--5 toestanden
 Signal presentState, nextState : state := state1;
 
-begin																					--Finite state machine (Moore - houd enkel rekening met huidige toestand)
+begin										--Finite state machine (Moore - houd enkel rekening met huidige toestand)
 StateRegister : process(sysClk)
 	begin
 		if rising_edge(sysClk) then
@@ -50,40 +50,39 @@ Uitgangen : process(presentState)
 NxtState : process(presentState, nextMode)
 	begin
 		case presentState is
-			when state1 =>															--Modus: tijdfunctie (Default state)
+			when state1 =>						--Modus: tijdfunctie (Default state)
 				if nextMode = '1' then
 					nextState <= state2;
 				else
 					nextState <= state1;
 				end if;
-			when state2 =>															--Modus: datumfunctie
+			when state2 =>						--Modus: datumfunctie
 				if nextMode = '1' then
 					nextState <= state3;
 				else
 					nextState <= state2;
 				end if;
-			when state3 =>															--Modus: alarmfunctie
+			when state3 =>						--Modus: alarmfunctie
 				if nextMode = '1' then
 					nextState <= state4;
 				else
 					nextState <= state3;
 				end if;
-			when state4 =>															--Modus: timerfunctie
+			when state4 =>						--Modus: timerfunctie
 				if nextMode = '1' then
 					nextState <= state5;
 				else
 					nextState <= state4;
 				end if;
-			when state5 =>															--Modus: chronometerfunctie
+			when state5 =>						--Modus: chronometerfunctie
 				if nextMode = '1' then
 					nextState <= state1;
 				else
 					nextState <= state5;
 				end if;
-			when others =>															--Onbekende toestand: naar modus: tijdsfunctie
+			when others =>						--Onbekende toestand: naar modus: tijdsfunctie
 				nextState <= state1;
 		end case;
 	end process;
 
 end Behavioral;
-
