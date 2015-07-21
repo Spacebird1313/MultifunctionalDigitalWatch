2336153679537c0600c0db33ed5a1814aed6987f
Thomas Huybrechts <huybthomas@gmail.com>
2015-07-21 19:04:28 +0200

    Update PulsSelector.vhd
    Indent correction

--- src/PulsSelector.vhd
+++ src/PulsSelector.vhd
@@ -13,19 +13,19 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity PulsSelector is
 	Port ( sysClk : in STD_LOGIC;
-			 puls0 : in STD_LOGIC;
-			 puls1 : in STD_LOGIC;
-			 selectPuls : in STD_LOGIC;
+	       puls0 : in STD_LOGIC;
+	       puls1 : in STD_LOGIC;
+	       selectPuls : in STD_LOGIC;
 	       reset : in STD_LOGIC;
-			 pulsUit : out STD_LOGIC := '0');
+	       pulsUit : out STD_LOGIC := '0');
 end PulsSelector;
 
 architecture Behavioral of PulsSelector is
 
-Type state is (state1, state2);							--2 toestanden
+Type state is (state1, state2);						--2 toestanden
 Signal presentState, nextState : state := state1;
 
-begin																--Finite state machine (Mealy - uitgang afhankelijk van de huidige stand en ingangen)
+begin									--Finite state machine (Mealy - uitgang afhankelijk van de huidige stand en ingangen)
 StateRegister : process(sysClk)
 	begin
 		if rising_edge(sysClk) then
@@ -40,13 +40,13 @@ StateRegister : process(sysClk)
 NxtState : process(presentState, selectPuls)
 	begin
 		case presentState is									
-			when state1 =>										--Stuur puls 0 door
+			when state1 =>					--Stuur puls 0 door
 				if selectPuls = '1' then
 					nextState <= state2;
 				else
 					nextState <= state1;
 				end if;
-			when others =>										--Stuur puls 1 door
+			when others =>					--Stuur puls 1 door
 				if selectPuls <= '0' then
 					nextState <= state1;
 				else
@@ -55,8 +55,8 @@ NxtState : process(presentState, selectPuls)
 		end case;
 	end process;
 
-pulsUit <= puls0 when presentState = state1 else   --Uitgang FSM
-			  puls1;
-							
+pulsUit <= puls0 when presentState = state1 else   			--Uitgang FSM
+	   puls1;
+	
 end Behavioral;
 
