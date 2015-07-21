1e8a1a7209b71d791dba75dd8592a580dec1c955
Thomas Huybrechts <huybthomas@gmail.com>
2015-07-21 15:55:34 +0200

    Update Deler.vhd
    Indent correction

--- src/Deler.vhd
+++ src/Deler.vhd
@@ -13,8 +13,8 @@ use ieee.numeric_std.ALL;
 
 entity Deler is
     Generic ( max : positive := 10);
-	 Port ( sysClk : in  STD_LOGIC;
-           enable : in  STD_LOGIC;
+    Port ( sysClk : in  STD_LOGIC;
+      	   enable : in  STD_LOGIC;
            CT : out  STD_LOGIC := '0');
 end Deler;
 
@@ -23,7 +23,7 @@ architecture Behavioral of Deler is
 Signal count : integer range 0 to max := 0;
 
 begin
---DIVIDE: tel aantal impulsen (max) waarna één puls wordt gestuurd (CT)
+--DIVIDE: tel aantal impulsen (max) waarna Ã©Ã©n puls wordt gestuurd (CT)
 div : process(sysClk)
 	begin
 		if rising_edge(sysClk) then
